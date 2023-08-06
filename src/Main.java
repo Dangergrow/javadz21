c6b6b121ac78dfe26df76d24b853aeb1537b6e45
Dangergrow <Dangergrow@gmail.com>
2023-08-06 11:09:03 +0300

    Added

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 100; //Начальный счет
+        int b = 1100; //Сумма пополнения
+        int bonus; //Сумма бонусов
+        int balance; //Общий баланс
+            if (b>= 1000) {bonus = b / 100;}
+            else {bonus = 0;}
+                System.out.println(balance= a + b + bonus);
+
+    }
+}
